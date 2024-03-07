a42a1b22b669d260c0eb1464553e2d56debca2b0
yawsamcode <samuelacheampong108@gmail.com>
2024-03-07 12:40:27 +0000

    Added background pattern

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,10 +14,18 @@ module.exports = {
         'Dark-gray': 'hsl(0, 0%, 59%)'
       }, 
       backgroundImage: {
-          "pattern-card": "url('./images/bg-pattern-card.svg')"
+          "pattern-card": "url('./images/bg-pattern-card.svg')",
+          "pattern-bottom": "url('./images/bg-pattern-bottom.svg')",
+          "pattern-top": "url('./images/bg-pattern-top.svg')",
       }
     },
     screens: {
+      '4xl': {'max': '2024px'},
+      // => @media (max-width: 2024px) { ... }
+
+      '3xl': {'max': '1791px'},
+      // => @media (max-width: 1791px) { ... }
+      
       '2xl': {'max': '1535px'},
       // => @media (max-width: 1535px) { ... }
 
@@ -32,6 +40,8 @@ module.exports = {
 
       'sm': {'max': '639px'},
       // => @media (max-width: 639px) { ... }
+
+      
     }
   },
   plugins: [],
